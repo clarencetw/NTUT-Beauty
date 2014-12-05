8800a8f5527cd38f92d87e9080d7dbf694164b68
Clarence <mr.lin.clarence@gmail.com>
2014-12-06 00:27:21 +0800

    delete file

--- config/appid.php
+++
@@ -1,7 +0,0 @@
-<?php
-$config = array(
-  'appId' => 'your app id',
-  'secret' => 'your app secret',
-  'allowSignedRequest' => false // optional but should be set to false for non-canvas apps
-);
-?>
